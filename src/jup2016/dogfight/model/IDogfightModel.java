4b1b5b8665e61b78131a2de928b8f9e6ab72bf43
Asus <Asus@Asus-PC>
2016-05-24 09:56:56 +0200

    Phase 2

---
+++ src/jup2016/dogfight/model/IDogfightModel.java
@@ -0,0 +1,8 @@
+package jup2016.dogfight.model;
+
+public interface IDogfightModel {
+	public abstract int IArea();
+	public abstract int IMobile();
+	public abstract int IDimension();
+	public abstract int Direction();
+}
